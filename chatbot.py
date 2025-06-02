9d4728030911cb8b75b66849e16815f4ebd204c6
Bishal Babu Dumre <139268131+BishalBabuDumre@users.noreply.github.com>
2025-06-02 23:44:22 +0000

    commit

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import pickle
 
 class NLPChatbot:
     def __init__(self):
